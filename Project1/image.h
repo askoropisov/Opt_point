df5c09aeb359f7a4f80004d1df62d600dda94b0e
askoropisov <askoropisov@s-terra.ru>
2021-06-09 15:10:44 +0300

    Добавил возможность генерации рандомных значений и коментарии по каждой строчке

--- Project1/image.h
+++ Project1/image.h
@@ -1,3 +1,6 @@
+//Этот файл является подключаемой пользовательский библиотекой.
+//Так часто делают, чтоб не писать много кода в одном файле
+//по сути этот файл отвечает как раз таки за визуализацию твоей ДРП
 #pragma once
 #include <iostream>
 #include <iomanip>
@@ -14,13 +17,12 @@
 using namespace std;                                                    // рабочее пространство имен
 
 int n;                                                                  //объявление размера ДРП
-int newDRP[1000][1000];                                               //объявление
-char Lboard[1000][1000], Sboard[1000][1000];
+int newDRP[1000][1000];                                                 //объявление числового ДРП
+char Lboard[1000][1000], Sboard[1000][1000];                            //объявление символьного ДРП
 
 
-void RenderScene() {
+void RenderScene() {                                                    //Это основная функция работы с графикой
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glViewport(0, 0, w1, h1);
     glMatrixMode(GL_PROJECTION);
@@ -29,41 +31,42 @@ void RenderScene() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glTranslatef(a, b, -Zoom);
-    glRotatef(vx, 1, 0, 0);
+    glTranslatef(a, b, -Zoom);                                          //функция, отвещающая за перемещение графического объекта (всей сцены) по 3 осям
+    glRotatef(vx, 1, 0, 0);                                             //это 3 функции отвещающие за вращение сцены по 3 осям    
     glRotatef(vy, 0, 1, 0);
     glRotatef(vz, 0, 0, 1);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)                                         //двойным циклом пробегаемся по нашему ДРП
     {
         for (int j = 0; j < n; j++)
         {
-            switch (newDRP[i][j])
+            switch (newDRP[i][j])                                       //рассматриваем каждый символ поочередно
             {
-            case 0: glBegin(GL_QUADS);							
-                glColor3f(1, 1, 1);
+            case 0: glBegin(GL_QUADS);		                            //если символ является 0, т.е пустой клеткой, по рисуем на его месте белый квадрат					
+                glColor3f(1, 1, 1);                                     //задаем цвет рисунка в формате RGB (если все 1, то это белый)
+                glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);         //4 строчки отвечающие за 4 вершины квадрата
+                glVertex3f(2 * j - 16.5, -2 * i + 16.5, 2);
+                glVertex3f(2 * j - 16.5, -2 * i - 1 + 16.5, 2);
+                glVertex3f(2 * j + 1 - 16.5, -2 * i - 1 + 16.5, 2);
+                glEnd();                                                //конец отрисовки одного объекта
+                break;
+            case 999: glBegin(GL_QUADS);		                        //если встречаем 999, т.е препятствие, то рисуем красный квадрат				
+                glColor3f(1, 0, 0);                                     //задаем красный цвет
                 glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);
                 glVertex3f(2 * j - 16.5, -2 * i + 16.5, 2);
                 glVertex3f(2 * j - 16.5, -2 * i - 1 + 16.5, 2);
                 glVertex3f(2 * j + 1 - 16.5, -2 * i - 1 + 16.5, 2);
                 glEnd();
                 break;
-            case 999: glBegin(GL_QUADS);						
-                glColor3f(1, 0, 0);
+            case -1: glBegin(GL_QUADS);			                        //если встречаем -1, т.е город, то рисуем зеленую пирамиду. Пирамиду рисуем, потому что просто захотелось нарисовать пирамиду
+                //рисуем квадрат как основание пирамиды
+                glColor3f(0, 1, 0);                                     //задаем цвет
                 glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);
                 glVertex3f(2 * j - 16.5, -2 * i + 16.5, 2);
                 glVertex3f(2 * j - 16.5, -2 * i - 1 + 16.5, 2);
                 glVertex3f(2 * j + 1 - 16.5, -2 * i - 1 + 16.5, 2);
                 glEnd();
-                break;
-            case -1: glBegin(GL_QUADS);							
-                glColor3f(0, 1, 0);
-                glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);
-                glVertex3f(2 * j - 16.5, -2 * i + 16.5, 2);
-                glVertex3f(2 * j - 16.5, -2 * i - 1 + 16.5, 2);
-                glVertex3f(2 * j + 1 - 16.5, -2 * i - 1 + 16.5, 2);
-                glEnd();
-
+                //рисуем 4 треугольника как стороны пирамиды
                 glBegin(GL_TRIANGLES);
                 glColor3f(0, 1, 0);
                 glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);
@@ -92,14 +95,14 @@ void RenderScene() {
                 glVertex3f(2 * j + 0.5 - 16.5, -2 * i + 16, 3);
                 glEnd();
                 break;
-            case -2: glBegin(GL_QUADS);							
-                glColor3f(0, 0, 1);
+            case -2: glBegin(GL_QUADS);		                                //если встречаем -2, т.е точку, то рисуем синюю пирамиду					
+                glColor3f(0, 0, 1);                                         //задаем цвет
                 glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);
                 glVertex3f(2 * j - 16.5, -2 * i + 16.5, 2);
                 glVertex3f(2 * j - 16.5, -2 * i - 1 + 16.5, 2);
                 glVertex3f(2 * j + 1 - 16.5, -2 * i - 1 + 16.5, 2);
                 glEnd();
-
+                //пирамида рисуется так же как и прошлая
                 glBegin(GL_TRIANGLES);
                 glColor3f(0, 0, 1);
                 glVertex3f(2 * j + 1 - 16.5, -2 * i + 16.5, 2);
@@ -128,32 +131,32 @@ void RenderScene() {
                 glVertex3f(2 * j + 0.5 - 16.5, -2 * i + 16, 3);
                 glEnd();
                 break;
-            case 777: glColor3f(1, 0, 1);						
-                glPushMatrix();
-                glTranslatef(2 * j - 16, -2 * i + 16, 3);
-                glutSolidSphere(1, 100, 100);
-                glPopMatrix();
+            case 777: glColor3f(1, 0, 1);	                                    //если встречаем 777 или оптимальную точку, то рисуем большой фиолетовый шар					
+                glPushMatrix();                                                 //говорим, что будем рисовать объемный объект
+                glTranslatef(2 * j - 16, -2 * i + 16, 3);                       //перемещаемся на некий коэфф
+                glutSolidSphere(1, 100, 100);                                   //рисуем сферу, радиуса 1. (остальные 2 аргумента отвечают за то, насколько круглым или угловатым будет шар)
+                glPopMatrix();                                                  //закончили работу с трехмерной графикой
                 break;
             }
         }
     }
-    glutSwapBuffers();
+    glutSwapBuffers();                                                          //запомнили все изменения, провели рендеринг
 }
 
 
-void print_termenal_and_barrier()
+void print_termenal_and_barrier()                                               //это функция вывода символьного представления нашего ДРП в консоль. По сути это не графика, но я решил перенести в этот файл
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)                                             //пробегаемся 2 циклом по ДРП
         {
-            cout << setw(2) << Lboard[i][j];
+            cout << setw(2) << Lboard[i][j];                                    //выводим каждый элемент матрицы с отступами 
         }
         cout << endl;
     }
 }
 
-void print_DRP()
+void print_DRP()                                                                //абсолютно такая же функция.....не помню, зачем я ее вводил, но лучше не удалять
 {
     for (int i = 0; i < n; i++)
     {
@@ -165,17 +168,20 @@ void print_DRP()
     }
 }
 
-void Print_newDRP(char Sboard[1000][1000])									
+void Print_newDRP(char Sboard[1000][1000])		                                //Функция превращающая ДРП символьного вида в ДРП числового вида							
 {
+    /*Сопоставляет каждого символу определенное число.
+    Это нужно, т.к не все функции могут работать с символами, а с числами могут*/
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)                                             //Просматриваем каждый элемент символьного ДРП
         {
-            if (Sboard[i][j] == '0') newDRP[i][j] = 0;
-            if (Sboard[i][j] == 'C') newDRP[i][j] = -1;
-            if (Sboard[i][j] == 'P') newDRP[i][j] = -2;
+            if (Sboard[i][j] == '0') newDRP[i][j] = 0;                          
+            if (Sboard[i][j] == 'C') newDRP[i][j] = -1;                         //если встречаем С, то задаем его значение = -1
+            if (Sboard[i][j] == 'P') newDRP[i][j] = -2;                         //аналогично с остальными символами
             if (Sboard[i][j] == '#') newDRP[i][j] = 999;
             if (Sboard[i][j] == '*') newDRP[i][j] = 777;
         }
     }
+    //это нужно как раз для того, чтоб функция RenderScene корректно работала с матрицей
 }
--- Project1/move.h
+++ Project1/move.h
@@ -1,4 +1,6 @@
-#pragma once
+//Этот файл является подключаемой пользовательский библиотекой.
+//Так часто делают, чтоб не писать много кода в одном файле
+//Он отвечает за малипуляцию получееного графического объектра с клавиатуры
 #pragma once
 #include <iostream>
 #include <iomanip>
@@ -11,37 +13,40 @@
 #pragma comment(lib, "glut32.lib")
 #pragma comment(lib, "glaux.lib")
 
-double w1 = 600.0, h1 = 600.0;
-float vx = 0.0, vy = 0.0, vz = 0.0;
-double Zoom = 50.0;
-double a, b;
+
+double w1 = 600.0, h1 = 600.0;							//размер окна графики по умолчанию
+float vx = 0.0, vy = 0.0, vz = 0.0;						// углы поворота по каждой оси по умолчанию
+double Zoom = 70.0;										//отдаление по умолчанию (коорд-та по z)
+double a, b;											// коор-ты изображения по х и у
 
 
-void Read_kb(unsigned char key, int, int)				// zoom and control
+void Read_kb(unsigned char key, int, int)				// функция восприятия нажатий на клавиши пользователем
 {
+	//проверяем нажатую клавифу. Если она равно какому-то из следующий символов, то
 	if (key == 's')
-		vx -= 2.0;
+		vx -= 2.0;										//вращаем по х
 	if (key == 'w')
 		vx += 2.0;
 	if (key == 'a')
-		vy -= 2.0;
+		vy -= 2.0;										//вращаем по у
 	if (key == 'd')
 		vy += 2.0;
 	if (key == '1')
-		vz -= 2.0;
+		vz -= 2.0;										//вращаем по  z
 	if (key == '3')
 		vz += 2.0;
-	if (key == '+')
+	if (key == '+')										//приближаем
 		Zoom -= 2.0;
-	if (key == '-')
+	if (key == '-')										//отдаляем
 		Zoom += 2.0;
 	if (key == 27)
-		exit(0);
-	glutPostRedisplay();
+		exit(0);										//на клавишу эскейп программа закроется
+	glutPostRedisplay();								//что-то типа "Объявляем функцию доверительной
 }
 
-void  ReadSpec(int key, int, int)						//
+void  ReadSpec(int key, int, int)						// восприятия нажатий на специальные клавиши пользователем
 {
+	//перемещение сцены по х и у стрелочками
 	if (key == GLUT_KEY_UP)
 		b += 1.0;
 	if (key == GLUT_KEY_DOWN)
@@ -50,11 +55,12 @@ void  ReadSpec(int key, int, int)						//
 		a += 1.0;
 	if (key == GLUT_KEY_LEFT)
 		a -= 1.0;
-	glutPostRedisplay();
+	glutPostRedisplay();								//что-то типа "Объявляем функцию доверительной
 }
 
-void reshape(int w, int h)								//window
+void reshape(int w, int h)								//какая-то обязательная функция для правильно отрисовки окна
 {
+	//ширина и высота окна
 	w1 = w;
 	h1 = h;
 	glutPostRedisplay();
